7653a06acf62edd27cea2a13540f79aab4eb90bd
Mark O Meara <markomeara98@gmail.com>
2024-05-14 21:28:38 +0100

    got status icons showing correctly

--- mark-note/src/renderer/src/components/NotePreview.tsx
+++ mark-note/src/renderer/src/components/NotePreview.tsx
@@ -1,6 +1,8 @@
 import { NoteInfo } from "@shared/models";
 import { ComponentProps } from "react";
 import { cn , formatDateFromMs} from '@renderer/utils'
+import { FaRegCircle, FaRegCircleDot, FaRegCirclePause, FaRegCircleCheck, FaRegCircleXmark } from "react-icons/fa6";
+
 
 export type NotePreviewProps = NoteInfo & {
     isActive?: boolean
@@ -17,6 +19,14 @@ export const NotePreview = ({
 }: NotePreviewProps) => {
     const date = formatDateFromMs(lastEditTime)
 
+    function findArrayElementByTitle(array, title) {
+        return array.find((element) => {
+          return element.title === title;
+        })
+    }
+
+    const chosenElement = findArrayElementByTitle(statusOptions, status)
+
     return( <div className={cn('cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75', 
     {
         'bg-zinc-400/75': isActive,
@@ -26,9 +36,39 @@ export const NotePreview = ({
     )}
     {...props}
     >
-        <h3 className="mb-1 font-bold truncate">{title}</h3>
+        <div className="flex space-x-2 items-center">
+            <div>{<chosenElement.icon className={chosenElement.className} />}</div>
+            <h3 className="font-bold truncate">{title}</h3>
+        </div>
         <span className="inline-block w-full mb-2 text-xs font-light text-left">{date}</span>
         <p>{status}</p>
     </div>
     )
 }
+
+
+export const statusOptions = [{
+    id: '0',
+    title: 'Active',
+    icon: FaRegCircleDot,
+    className: 'text-sky-400 w-3 h-3',
+  }, 
+  {
+    id: '1',
+    title: 'On Hold',
+    icon: FaRegCirclePause,
+    className: 'text-orange-400 w-3 h-3',
+  },
+  {
+    id: '2',
+    title: 'Completed',
+    icon: FaRegCircleCheck,
+    className: 'text-green-400 w-3 h-3',
+  },
+  {
+    id: '3',
+    title: 'Dropped',
+    icon: FaRegCircleXmark,
+    className: 'text-rose-400 w-3 h-3',
+  }
+];
